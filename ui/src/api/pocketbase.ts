cd30174fbfe9355d5b4d67f8567ba5c6f9c71cbf
Nasonov Viktor <qxuken@gmail.com>
2025-03-04 10:58:15 +0300

    test: expose pbclient

--- ui/src/api/pocketbase.ts
+++ ui/src/api/pocketbase.ts
@@ -2,3 +2,5 @@ import Pocketbase from 'pocketbase';
 
 export const pbClient = new Pocketbase(import.meta.env.VITE_POCKETBASE_URL);
 pbClient.autoCancellation(false);
+
+window.pbClient = pbClient;
--- ui/src/window.d.ts
+++ ui/src/window.d.ts
@@ -1,3 +1,4 @@
 interface Window {
   loadDevtools: () => void;
+  pbClient: import('pocketbase');
 }
